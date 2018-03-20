1b354cff911d815030b7c67301b69e050acee4b2
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-03-20 00:18:56 +0000

    Added text animation

--- 05-flex-panel-gallery/app.js
+++ 05-flex-panel-gallery/app.js
@@ -1,10 +1,8 @@
-const panelText = document.querySelectorAll('.panel');
+const panel = document.querySelectorAll('.panel');
 
-const textIn = function() {
+const flexOpen = function() {
   console.log('Hello');
   this.classList.toggle('open');
 }
 
-panelText.forEach((text) => {
-  text.addEventListener('click', textIn);
-})
+panel.forEach((text) => { text.addEventListener('click', flexOpen) })
